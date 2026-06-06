d42ecd050820f16277598e9c16463fece4302a3f
felipebottega <felipebottega@gmail.com>
2026-06-05 22:02:59 -0300

    feat: simplifying pipeline

--- ComfyUI/scripts/run_workflow.py
+++ ComfyUI/scripts/run_workflow.py
@@ -5,57 +5,31 @@ import time
 import requests
 from urllib import request
 
-
 SERVER_URL = "http://127.0.0.1:8188"    # Local server address.
+
+if len(sys.argv) < 2:
+    sys.exit("Usage: python run_workflow.py <path_to_workflow.json>")
+
 workflow_filepath = sys.argv[1]
-parameters_filepath = sys.argv[2]
 
-
-def update_workflow(workflow, params):
-    for key, value in params.items():
-        if key in ["path_to_input", "path_to_output"]:
-            continue
-        if key not in workflow:
-            raise KeyError(f"Workflow key not found: {key}")
-        if isinstance(value, dict):
-            workflow.setdefault(key, {})
-            update_workflow(workflow[key], value)
-        else:
-            workflow[key] = value   
-
-
-# Loads the workflow from the JSON file (workflows-api).
+# Loads the workflow directly from the JSON file.
 with open(workflow_filepath, "r", encoding="utf-8") as f:
     workflow = json.load(f)
     
-# Loads the parameters.
-with open(parameters_filepath, "r", encoding="utf-8") as f:
-    params = json.load(f)
-    
-    if "path_to_input" not in params.keys():
-        sys.exit("Parameter path_to_input missing")
-    else:
-        for x in workflow:
-            if workflow[x]['class_type'] == 'LoadImage':
-                workflow[x]['inputs']['image'] = params["path_to_input"]
-            
-    if "path_to_output" not in params.keys():
-        sys.exit("Parameter path_to_output missing")
-    else:
-        for x in workflow:
-            if workflow[x]['class_type'] == 'SaveVideo':
-                workflow[x]['inputs']['filename_prefix'] = params["path_to_output"]
+# Dynamically checks if the workflow contains an input node for logging purposes.
+input_log = "Direct execution"
+for x in workflow:
+    if workflow[x].get('class_type') == 'LoadImage':
+        input_log = workflow[x]['inputs'].get('image', 'Image Node')
 
-update_workflow(workflow, params)
-        
 p = {"prompt": workflow}
 data = json.dumps(p).encode('utf-8')
 
-# Send workflow to execution.
-req =  request.Request(f"{SERVER_URL}/prompt", data=data)
+# Send workflow directly to execution.
+req = request.Request(f"{SERVER_URL}/prompt", data=data)
 resp = request.urlopen(req)
 prompt_id = json.load(resp)["prompt_id"]
-print(f'Executing {prompt_id} - {params["path_to_input"]}')
+print(f'Executing {prompt_id} - {input_log}')
 
 # Wait to finish.
 while True:
